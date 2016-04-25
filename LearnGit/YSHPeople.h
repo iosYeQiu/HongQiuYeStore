93226be040e4902b0c09bd0581a18560cd69e991
mac <mac@laoshidekejian2.local>
2016-04-25 18:14:13 +0800

    haha

---
+++ LearnGit/YSHPeople.h
@@ -0,0 +1,13 @@
+//
+//  YSHPeople.h
+//  LearnGit
+//
+//  Created by mac on 16/4/25.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YSHPeople : NSObject
+/*******/
+@end
---
+++ LearnGit/YSHPeople.m
@@ -0,0 +1,16 @@
+//
+//  YSHPeople.m
+//  LearnGit
+//
+//  Created by mac on 16/4/25.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import "YSHPeople.h"
+
+@implementation YSHPeople
+-(void)people
+{
+    NSLog(@"chifan ");
+}
+@end
